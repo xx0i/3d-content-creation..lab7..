5e9d4daeabfefb786a50cee2c8c2d3accf4a3187
unknown <natalie9705@gmail.com>
2024-10-17 18:37:00 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -118,7 +118,8 @@ public:
 		//loadingRudimentaryfromGltf("C:/full sail/3d content creation/3dcc-lab-5-xx0i/Models/triangle.gltf");
 		//loadingRudimentaryfromGltf("C:/full sail/3d content creation/3dcc-lab-5-xx0i/Models/triangle_blender.gltf");
 		//loadingRudimentaryfromGltf("C:/full sail/3d content creation/3dcc-lab-5-xx0i/Models/cat_blender.gltf");
-		loadingRudimentaryfromGltf("C:/full sail/3d content creation/3dcc-lab-6-xx0i/Models/fish_blender.gltf");
+		//loadingRudimentaryfromGltf("C:/full sail/3d content creation/3dcc-lab-6-xx0i/Models/fish_blender.gltf");
+		loadingRudimentaryfromGltf("C:/full sail/3d content creation/3dcc-lab-7-xx0i/Models/WaterBottle/bottle_blender.gltf");
 		loadImageFromGltf();
 		createDescriptorLayout();
 		InitializeGraphics();
