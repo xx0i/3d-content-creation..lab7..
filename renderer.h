8aba7731bb242803958369e8e45a73b8a5a2afd1
unknown <natalie9705@gmail.com>
2024-10-17 19:23:09 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -132,7 +132,7 @@ public:
 		uint32_t currentImage;
 		vlk.GetSwapchainCurrentImage(currentImage);
 
-		GW::MATH::GVECTORF cameraPosition = { -0.75f, 0.25f, 0.5f };
+		GW::MATH::GVECTORF cameraPosition = { 1.0f, 0.25f, 0.5f };
 		GW::MATH::GVECTORF targetPosition = { 0.0f, 0.02f, 0.0f };
 		GW::MATH::GVECTORF upVector = { 0.0f, 1.0f, 0.0f };
 		interfaceProxy.LookAtLHF(cameraPosition, targetPosition, upVector, viewMatrix);
